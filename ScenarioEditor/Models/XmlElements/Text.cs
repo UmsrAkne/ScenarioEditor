f93509b81674101519fa03779fde19a86694b420
Akn <sibata486@gmail.com>
2022-12-04 15:49:47 +0900

    add / XElement から Text インスタンスを生成する機能を実装

--- ScenarioEditor/Models/XmlElements/Text.cs
+++ ScenarioEditor/Models/XmlElements/Text.cs
@@ -1,3 +1,4 @@
+using System.Xml.Linq;
 using System.Xml.Serialization;
 
 namespace ScenarioEditor.Models.XmlElements
@@ -5,7 +6,34 @@ namespace ScenarioEditor.Models.XmlElements
     [XmlRoot("text")]
     public class Text
     {
+        public Text()
+        {
+        }
+
+        public Text(XElement scenarioElement)
+        {
+            var textElement = scenarioElement.Element(ElementName);
+
+            var str = textElement?.Attribute(StrAttribute);
+            if (str != null)
+            {
+                Str = str.Value;
+            }
+
+            var stringAttribute = textElement?.Attribute(StringAttribute);
+            if (stringAttribute != null)
+            {
+                Str = stringAttribute.Value;
+            }
+        }
+
         [XmlAttribute("str")]
         public string Str { get; set; }
+
+        public string ElementName { get; set; } = "text";
+
+        private string StrAttribute { get; } = "str";
+
+        private string StringAttribute { get; } = "string";
     }
 }
---
+++ Tests/Models/XmlElements/TextTest.cs
@@ -0,0 +1,26 @@
+using System.Xml.Linq;
+using NUnit.Framework;
+using ScenarioEditor.Models.XmlElements;
+
+namespace Tests.Models.XmlElements
+{
+    [TestFixture]
+    public class TextTest
+    {
+        [Test]
+        public void デフォルトコンストラクタのテスト()
+        {
+            var t = new Text();
+        }
+
+        [Test]
+        public void コンストラクタのテスト()
+        {
+            var xml1 = XDocument.Parse("<scenario><text string=\"テスト用テキスト１\" /></scenario>").Element("scenario");
+            var xml2 = XDocument.Parse("<scenario><text str=\"テスト用テキスト２\" /></scenario>").Element("scenario");
+
+            Assert.AreEqual(new Text(xml1).Str, "テスト用テキスト１");
+            Assert.AreEqual(new Text(xml2).Str, "テスト用テキスト２");
+        }
+    }
+}
