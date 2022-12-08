5c3a568907dc590d7dfe3efba0ac09ab059c0167
Akn <sibata486@gmail.com>
2022-12-08 19:02:54 +0900

    add / Draw 要素を表すクラスを追加

---
+++ ScenarioEditor/Models/XmlElements/Draw.cs
@@ -0,0 +1,85 @@
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using Prism.Mvvm;
+
+namespace ScenarioEditor.Models.XmlElements
+{
+    [XmlRoot("image")]
+    public class Draw : BindableBase
+    {
+        private string a = string.Empty;
+        private string b = string.Empty;
+        private string c = string.Empty;
+        private string d = string.Empty;
+        private string target = "main";
+
+        public Draw()
+        {
+        }
+
+        public Draw(XElement scenarioElement)
+        {
+            var imageElement = scenarioElement.Element(ElementName);
+
+            var imageNameA = imageElement?.Attribute(AAttribute);
+            A = imageNameA != null ? imageNameA.Value : string.Empty;
+
+            var imageNameB = imageElement?.Attribute(BAttribute);
+            B = imageNameB != null ? imageNameB.Value : string.Empty;
+
+            var imageNameC = imageElement?.Attribute(CAttribute);
+            C = imageNameC != null ? imageNameC.Value : string.Empty;
+
+            var imageNameD = imageElement?.Attribute(DAttribute);
+            D = imageNameD != null ? imageNameD.Value : string.Empty;
+
+            var tg = imageElement?.Attribute(TargetAttribute);
+            Target = tg != null ? tg.Value : string.Empty;
+        }
+
+        public string ElementName => "draw";
+
+        public string A { get => a; set => SetProperty(ref a, value); }
+
+        public string B { get => b; set => SetProperty(ref b, value); }
+
+        public string C { get => c; set => SetProperty(ref c, value); }
+
+        public string D { get => d; set => SetProperty(ref d, value); }
+
+        public string Target { get => target; set => SetProperty(ref target, value); }
+
+        public bool IsDefault => A + B + C + D == string.Empty;
+
+        private string AAttribute => "a";
+
+        private string BAttribute => "b";
+
+        private string CAttribute => "c";
+
+        private string DAttribute => "d";
+
+        private string ScaleAttribute => "scale";
+
+        private string XAttribute => "x";
+
+        private string YAttribute => "y";
+
+        private string TargetAttribute => "target";
+
+        public override string ToString()
+        {
+            if (IsDefault)
+            {
+                return string.Empty;
+            }
+
+            return $"<draw " +
+                   $"{AAttribute}=\"{A}\" " +
+                   $"{BAttribute}=\"{B}\" " +
+                   $"{CAttribute}=\"{C}\" " +
+                   $"{DAttribute}=\"{D}\" " +
+                   $"{TargetAttribute}=\"{Target}\" " + $"/>";
+        }
+    }
+}
---
+++ Tests/Models/XmlElements/DrawTest.cs
@@ -0,0 +1,62 @@
+using System.Xml.Linq;
+using NUnit.Framework;
+using ScenarioEditor.Models.XmlElements;
+
+namespace Tests.Models.XmlElements
+{
+    [TestFixture]
+    public class DrawTest
+    {
+        [Test]
+        public void 引数なしコンストラクタのテスト()
+        {
+            var _ = new Draw();
+        }
+
+        [Test]
+        public void コンストラクタのテスト()
+        {
+            var xmlText = "<scenario>" +
+                          "<draw " +
+                          "a=\"imageA\" b=\"imageB\" c=\"imageC\" d=\"imageD\" " +
+                          "x=\"100\" y=\"200\" " +
+                          "scale=\"1.5\" target=\"front\" />" +
+                          "</scenario>";
+
+            var draw = new Draw(XElement.Parse(xmlText));
+
+            Assert.AreEqual("imageA", draw.A);
+            Assert.AreEqual("imageB", draw.B);
+            Assert.AreEqual("imageC", draw.C);
+            Assert.AreEqual("imageD", draw.D);
+
+            Assert.AreEqual("front", draw.Target);
+        }
+
+        [Test]
+        public void Draw要素を含まない場合のテスト()
+        {
+            // Draw要素を含まない場合も例外が出ないか確認
+            var _ = new Draw(XElement.Parse("<scenario></scenario>"));
+        }
+
+        [Test]
+        public void ToStringのテスト()
+        {
+            var xmlText = "<scenario>" +
+                          "<draw " +
+                          "a=\"imageA\" b=\"imageB\" c=\"imageC\" d=\"imageD\" " +
+                          "target=\"front\" />" +
+                          "</scenario>";
+
+            Assert.AreEqual(
+                "<draw a=\"imageA\" b=\"imageB\" c=\"imageC\" d=\"imageD\" target=\"front\" />",
+                new Draw(new XElement(XElement.Parse(xmlText))).ToString()
+            );
+
+            // Draw の情報が無い場合は 空文字が返る
+            Assert.AreEqual(string.Empty, new Draw(XElement.Parse("<scenario></scenario>")).ToString());
+            Assert.AreEqual(string.Empty, new Draw().ToString());
+        }
+    }
+}
